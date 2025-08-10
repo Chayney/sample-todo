80204569a7ed8622b5a21a537b0eae888257b5cd
Chayney <chunchunmaru33333@gmail.com>
2025-08-11 04:42:52 +0900

    一時的対処

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,16 @@ import Link from 'next/link';
 import { useState } from 'react';
 
 export default function Home() {
-  const [text, setText] = useState('');
-  const [todo, setTodo] = useState<Todo[]>([]);
+  const [_text, _setText] = useState('');
+  const [_todo, _setTodo] = useState<Todo[]>([]);
 
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => setText(event.target.value);
+  // const onChange = (event: React.ChangeEvent<HTMLInputElement>) => setText(event.target.value);
 
   return (
     <>
       <h1>Todoアプリ</h1>
       <div>
-        <input placeholder='タスクを入力' onChange={onChange} />
+        <input placeholder='タスクを入力' />
         <button>追加</button>
       </div>
       <Link href={'/'}>
