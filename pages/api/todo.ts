0261f38a3e656fadc688509a6ec4e590c1449070
Chayney <chunchunmaru33333@gmail.com>
2025-08-11 08:22:19 +0900

    POSTメソッドの追加

--- pages/api/todo.ts
+++ pages/api/todo.ts
@@ -1,18 +1,33 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Todo } from '../../types/todo';
+import { PrismaClient } from "../../prisma/generated/prisma";
 
-// eslint-disable-next-line prefer-const
-let todos: Todo[] = [{
-  id: 0,
-  text: '',
-  completed: false
-}];
+const prisma = new PrismaClient();
 
-export default function handler(
+/* eslint-disable */
+let todos: Todo[] = [];
+/* eslint-enable */
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   if (req.method === 'GET') {
-    res.status(200).json(todos);
+    try {
+      todos = await prisma.todo.findMany();
+      res.status(200).json(todos);
+    } catch (error) {
+      console.error("Failed to fetch todos:", error);
+      res.status(500).json({ error: "Failed to fetch todos" });
+    }
+  } else if (req.method === 'POST') {
+    const { text } = req.body;
+    const newTodo: Todo = await prisma.todo.create({
+      data: {
+        text: text,
+        completed: false
+      }
+    });
+    res.status(200).json(newTodo);
   }
 }
