2fef74438a9d87c00e03475a88eb92733deae1df
Chayney <chunchunmaru33333@gmail.com>
2025-08-11 05:04:05 +0900

    一時的対処

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,28 @@
 import { Todo } from '../types/todo';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  // const [text, setText] = useState('');
-  // const [todo, setTodo] = useState<Todo[]>([]);
+  const [text, setText] = useState('');
+  const [todo, setTodo] = useState<Todo[]>([]);
 
-  // const onChange = (event: React.ChangeEvent<HTMLInputElement>) => setText(event.target.value);
+  const fetchTodos = async () => {
+    const res = await fetch('/api/todo');
+    const data: Todo[] = await res.json();
+    setTodo(data);
+  }
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => setText(event.target.value);
 
   return (
     <>
       <h1>Todoアプリ</h1>
       <div>
-        <input placeholder='タスクを入力' />
+        <input placeholder='タスクを入力' onChange={onChange} />
         <button>追加</button>
       </div>
       <Link href={'/'}>
